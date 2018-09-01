46620bf6a6a1ca976f9c78e9e59aa4356c7b4c17
rakar <rich.kopelow@gmail.com>
2018-09-01 12:31:41 -0400

    Initial project setup with cyborg

---
+++ src/main/java/frc/robot/RobotCB.java
@@ -0,0 +1,31 @@
+package frc.robot;
+
+import org.montclairrobotics.cyborg.Cyborg;
+
+public class RobotCB extends Cyborg {
+
+    @Override
+    public void cyborgAutonomousInit() {
+
+    }
+
+    @Override
+    public void cyborgDisabledInit() {
+
+    }
+
+    @Override
+    public void cyborgInit() {
+
+    }
+
+    @Override
+    public void cyborgTeleopInit() {
+
+    }
+
+    @Override
+    public void cyborgTestInit() {
+
+    }
+}
